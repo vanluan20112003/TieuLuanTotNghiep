12965c678c9cd4bfb5714d3039213fa31526ee0b
vanluan20112003 <vanluan.9a9@gmail.com>
2025-01-10 19:44:04 +0700

    Initial commit for Laravel app

--- public/index.php
+++ public/index.php
@@ -1,17 +1,25 @@
 <?php
 
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell
+ */
+
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+/**
+ * The Laravel framework bootstrap file.
+ */
+require __DIR__.'/../bootstrap/app.php';
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+$kernel = $app->make(Kernel::class);
+$response = $kernel->handle(
+    $request = Request::capture()
+);
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+$response->send();
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$kernel->terminate($request, $response);
